613e6a1656a685b0f679f2eada11cc5d366c016e
OfficialCRUGG <ich@crugg.de>
2021-08-10 23:33:33 +0200

    feat: Split home to /landing and /explore

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <Container>
-    <h1>Hello World</h1>
-  </Container>
+  
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  mounted() {
+    switch(localStorage.getItem("homePage")) {
+      case "explore":
+        this.$router.push("/explore");
+        break;
+      default:
+        this.$router.push("/landing");
+    }
+  }
+});
 </script>
