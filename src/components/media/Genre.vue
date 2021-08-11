a9caa6888fd273c62bd14b22f01cbfea2d90b791
OfficialCRUGG <ich@crugg.de>
2021-08-11 18:11:06 +0200

    feat: Toggle for spoiler tags

--- src/components/media/Genre.vue
+++ src/components/media/Genre.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rounded shadow bg-gray-900 dark:bg-dark-900 py-1 px-2 hover:pl-3 cursor-pointer transition-all duration-250 flex items-center whitespace-nowrap md:whitespace-normal">
-    <p class="w-full flex-grow">{{ text }}</p>
+    <p :class="`w-full flex-grow ${ spoiler ? 'text-red-500' : '' }`">{{ text }}</p>
     <p v-if="rank" class="text-xs text-gray-500 pl-2">{{ rank }}%</p>
   </div>
 </template>
@@ -10,7 +10,8 @@ import Vue from 'vue'
 export default Vue.extend({
   props: {
     text: String,
-    rank: String
+    rank: String,
+    spoiler: Boolean
   }
 });
 </script>
--- src/components/media/MediaTags.vue
+++ src/components/media/MediaTags.vue
@@ -2,6 +2,7 @@
     <Section class="overflow-x-scroll md:overflow-hidden">
         <h2 class="text-lg font-semibold mb-1">{{ $t("media.section.tags") }}</h2>
         <div class="flex md:flex-col space-x-1 md:space-x-0 md:space-y-1">
+            <Genre v-for="tag in filteredTags" :key="tag.id" :text="tag.name" :rank="tag.rank.toString()" :spoiler="tag.isMediaSpoiler || tag.isGeneralSpoiler" />
             <p class="text-gray-500 text-sm cursor-pointer" @click="showSpoilers = !showSpoilers">{{ $t("media.tags.toggleSpoilers") }}</p>
         </div>
     </Section>
@@ -13,5 +14,20 @@ export default Vue.extend({
     props: {
         tags: Array,
     },
+    data() {
+        return {
+            showSpoilers: false
+        }
+    },
+    computed: {
+        filteredTags(): any {
+            if(this.showSpoilers) return this.tags;
+            return this.tags.filter((tag: any) => {
+                if(tag.isMediaSpoiler) return false;
+                if(tag.isGeneralSpoiler) return false;
+                return true;
+            });
+        }
+    }
 });
 </script>
