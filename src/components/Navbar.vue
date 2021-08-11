1ee11dba35e147ac84efd24fe56f7a4b199a97f4
OfficialCRUGG <ich@crugg.de>
2021-08-11 15:59:37 +0200

    style: Fixed formatting of Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="bg-gray-800 dark:bg-dark-800 shadow-sm py-4 mb-5">
-    <Container>
-      <div class="flex items-center">
-        <div class="flex-grow w-1/5 text-left">
-          <nuxt-link to="/"><img src="/assets/svg/logo.svg" class="h-8" /></nuxt-link>
-        </div>
-        <div class="w-full flex-grow flex justify-center items-center space-x-6">
-          <nuxt-link to="/">Home</nuxt-link>
-          <nuxt-link to="/search">Search</nuxt-link>
-          <nuxt-link to="/anime/101291">Example Anime</nuxt-link>
-        </div>
-        <div class="flex-grow w-1/5 text-right">
-          
-        </div>
-      </div>
-    </Container>
-  </div>
+    <div class="bg-gray-800 dark:bg-dark-800 shadow-sm py-4 mb-5">
+        <Container>
+            <div class="flex items-center">
+                <div class="flex-grow w-1/5 text-left">
+                    <nuxt-link to="/"><img src="/assets/svg/logo.svg" class="h-8" /></nuxt-link>
+                </div>
+                <div class="w-full flex-grow flex justify-center items-center space-x-6">
+                    <nuxt-link to="/">Home</nuxt-link>
+                    <nuxt-link to="/search">Search</nuxt-link>
+                    <nuxt-link to="/anime/101291">Example Anime</nuxt-link>
+                </div>
+                <div class="flex-grow w-1/5 text-right">
+                </div>
+            </div>
+        </Container>
+    </div>
 </template>
