2f52d6cae0467f85cad4b5bc39e7e0378a185417
OfficialCRUGG <ich@crugg.de>
2021-08-14 16:31:51 +0200

    feat: Added Imprint and Privacy Policy

---
+++ src/pages/imprint.vue
@@ -0,0 +1,35 @@
+<template>
+    <Container class="mt-5">
+        <h1 class="text-3xl font-bold mb-1">{{ $t("imprint.heading") }}</h1>
+        <h2 class="text-xl font-semibold mb-3">{{ $t("imprint.accordingTo") }}</h2>
+        <p class="font-semibold">{{ $t("imprint.responsible") }}</p>
+        <p>
+            Dennis Paulus<br>
+            Voerder Straße 99 (rear building)<br>
+            58135 Hagen<br>
+            {{ $t("imprint.germany") }}
+        </p>
+        <p class="mt-1"><b>E-Mail:</b> hello@crugg.de</p>
+
+        <h1 class="text-2xl font-bold mb-2 mt-10">{{ $t("imprint.disclaimers") }}</h1>
+        <div v-for="disclaimer in disclaimers" :key="disclaimer" class="mb-5">
+            <h2 class="text-lg font-semibold mb-1">{{ $t(`imprint.disclaimers.${disclaimer}.heading`) }}</h2>
+            <p>{{ $t(`imprint.disclaimers.${disclaimer}.text`) }}</p>
+            <p class="italic mt-1">{{ $t(`imprint.disclaimers.${disclaimer}.tldr`) }}</p>
+        </div>
+    </Container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    data() {
+        return {
+            disclaimers: [
+                "content",
+                "links"
+            ]
+        }
+    }
+});
+</script>
---
+++ src/pages/privacy.vue
@@ -0,0 +1,6 @@
+<template>
+    <Container class="mt-5">
+        <h1 class="text-3xl font-bold mb-1">{{ $t("privacy.heading") }}</h1>
+        <p v-html="$t('privacy.text', { netlify: `<a target='_blank' href='https://www.netlify.com/privacy/' class='underline'>${ $t('privacy.netlifyLink') }</a>`, anilist: `<a target='_blank' href='https://anilist.co/terms' class='underline'>${ $t('privacy.anilistLink') }</a>` })" />
+    </Container>
+</template>
