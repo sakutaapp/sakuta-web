ce4e0f93bdf5d17e449b1bd2387099794240b61a
OfficialCRUGG <ich@crugg.de>
2021-08-30 18:02:19 +0200

    💄 Changed blue theme to deep pink

--- windi.config.ts
+++ windi.config.ts
@@ -11,6 +11,18 @@ export default defineConfig({
                 primary: "#FF005B",
                 aniList: "#02A9FF",
                 mal: "#2E5A2",
+                gray: {
+                    50: "#f9fafb",
+                    100: "#f3f4f6",
+                    200: "#e5e7eb",
+                    300: "#d1d5db",
+                    400: "#9ca3af",
+                    500: "#6b7280",
+                    600: "#4b5563",
+                    700: "#374151",
+                    800: "#1f0818",
+                    900: "#170212",
+                },
             },
         },
         fontFamily: {
