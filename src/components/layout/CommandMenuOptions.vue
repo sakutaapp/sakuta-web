c0d0db132798cc2755261952345d0eb39f1b52e3
OfficialCRUGG <ich@crugg.de>
2021-09-04 16:18:17 +0200

    🐛 Command menu now scrolls when keyboard is used

--- src/components/layout/CommandMenuOptions.vue
+++ src/components/layout/CommandMenuOptions.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="border-t-1 border-white !border-opacity-5 max-h-132 overflow-y-scroll">
-        <div v-for="option in options" :key="option.name" :class="`${option.name === value ? 'bg-white bg-opacity-5' : ''} px-4 py-2 cursor-pointer relative`" @mouseover="$emit('input', option.name)" @click="$emit('enter')">
+    <div class="border-t-1 border-white !border-opacity-5 max-h-132 overflow-y-scroll" ref="commandMenuScroll">
+        <div v-for="option in options" :key="option.name" :ref="option.name" :class="`${option.name === value ? 'bg-white bg-opacity-5' : ''} px-4 py-2 cursor-pointer relative`" @mouseover="$emit('input', option.name)" @click="$emit('enter')">
             <div class="bg-primary absolute left-0 top-0 h-full w-0.8" v-if="option.name === value" />
             <div class="flex items-center space-x-3">
                 <svg v-if="page === ''" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" v-html="option.icon" />
@@ -35,6 +35,21 @@ export default Vue.extend({
                 propertyArray.push(this.$t("media.season", { season: this.$t(`media.season.${properties.season}`), year: properties.seasonYear }));
             }
             return propertyArray.join(" • ");
+        },
+    },
+    watch: {
+        value(newValue) {
+            if (!this.$refs[newValue]) return;
+            /// @ts-ignore
+            if (this.$refs[newValue][0].offsetTop - 85 + this.$refs[newValue][0].offsetHeight > this.$refs.commandMenuScroll.scrollTop + this.$refs.commandMenuScroll.offsetHeight) {
+                /// @ts-ignore
+                this.$refs.commandMenuScroll.scrollTop = this.$refs[newValue][0].offsetTop - 85;
+            }
+            /// @ts-ignore
+            if (this.$refs[newValue][0].offsetTop - 85 < this.$refs.commandMenuScroll.scrollTop) {
+                /// @ts-ignore
+                this.$refs.commandMenuScroll.scrollTop = this.$refs[newValue][0].offsetTop - 85;
+            }
         },
     },
 });
