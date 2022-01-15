29503d6baf89fb28c611444466d8d20a247dbf3e
OfficialCRUGG <ich@crugg.de>
2022-01-15 11:17:37 +0100

    📱 Fixed responsiveness issues in settings window

--- src/components/misc/SettingsSelector.vue
+++ src/components/misc/SettingsSelector.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex space-x-2">
-        <p v-for="option in options" :key="option" :class="`cursor-pointer transition duration-100 px-2.5 py-1 rounded ${option === value ? 'bg-white bg-opacity-10 hover:bg-opacity-15 text-white' : 'bg-white bg-opacity-0 hover:bg-opacity-5'}`" @click="$emit('input', option)">{{ lang ? $i18n.messages[option]["meta.lang"] : $t(`settings.option.${option}`) }}</p>
+    <div class="flex flex-wrap">
+        <p v-for="option in options" :key="option" :class="`cursor-pointer transition duration-100 px-2.5 py-1 mr-2 rounded ${option === value ? 'bg-white bg-opacity-10 hover:bg-opacity-15 text-white' : 'bg-white bg-opacity-0 hover:bg-opacity-5'}`" @click="$emit('input', option)">{{ lang ? $i18n.messages[option]["meta.lang"] : $t(`settings.option.${option}`) }}</p>
     </div>
 </template>
 
