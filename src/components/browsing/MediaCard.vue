52a2e379b58a4c1016f29f880376e161aa5aa94a
OfficialCRUGG <ich@crugg.de>
2022-04-10 18:07:24 +0200

    🐛 Fixed #7 and #8

--- src/components/browsing/MediaCard.vue
+++ src/components/browsing/MediaCard.vue
@@ -1,14 +1,14 @@
 <template>
-    <nuxt-link :to="`/${type}/${id}`" class="media-card">
-        <div class="w-full aspect-w-3 aspect-h-3.5 media-card-cover rounded-lg overflow-hidden transition-all duration-250" :style="`background-image: url('${coverImage}')`" @mouseenter="showInner = true" @mouseleave="showInner = false">
-            <div :class="`w-full h-full bg-dark-900 bg-opacity-70 backdrop-filter backdrop-blur-sm transition duration-250 p-2 md:p-3 lg:p-4 text-white text-opacity-80 media-card-cover-inner ${showInner ? 'opacity-100' : 'opacity-0'}`">
-                <h3 class="text-lg md:text-xl lg:text-2xl font-bold uppercase text-white text-opacity-100">{{ title }}</h3>
+    <nuxt-link :to="`/${type}/${id}`" class="media-card group">
+        <div class="w-full aspect-w-3 aspect-h-3.5 media-card-cover rounded-lg overflow-hidden transition-all duration-250" :style="`background-image: url('${coverImage}')`">
+            <div :class="`w-full h-full bg-dark-900 bg-opacity-70 backdrop-filter backdrop-blur-sm transition duration-250 p-2 md:p-3 lg:p-4 text-white text-opacity-80 media-card-cover-inner opacity-0 group-hover:opacity-100`">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold uppercase text-white text-opacity-100">{{ charLimit(title, 55) }}</h3>
                 <h3 v-if="format" class="text-sm md:text-base lg:text-lg font-bold uppercase">{{ $t(`media.format.${format}`) }}</h3>
                 <h4 v-if="status" class="text-xs md:text-sm font-semibold uppercase">{{ $t(`media.status.${status}`) }}</h4>
                 <h4 v-if="season && seasonYear" class="text-xs md:text-sm font-semibold uppercase">{{ $t(`media.season`, { season: $t(`media.season.${season}`), year: seasonYear }) }}</h4>
             </div>
         </div>
-        <p class="mt-1 media-card-text transition duration-250">{{ title }}</p>
+        <p class="mt-1 media-card-text transition duration-250">{{ charLimit(title, 45) }}</p>
     </nuxt-link>
 </template>
 
@@ -48,6 +48,11 @@ export default Vue.extend({
         },
         seasonYear(): any {
             return this.media?.seasonYear || undefined;
+        },
+    },
+    methods: {
+        charLimit(text: string, limit: number): string {
+            return text.length > limit ? `${text.substring(0, limit)}...` : text;
         },
     },
 });
