091c0a6db1b6bba8ecea766ba33299f2352f1fca
OfficialCRUGG <ich@crugg.de>
2022-04-12 01:26:58 +0200

    ✨ Updated languages

--- src/components/settings/LanguageOption.vue
+++ src/components/settings/LanguageOption.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="bg-dark-900 bg-opacity-50 hover:bg-opacity-100 rounded-full flex justify-between px-3 py-1.5 cursor-pointer transition duration-250" @click="$emit('select')">
-        <div class="flex space-x-2">
+        <div class="flex space-x-3">
             <div class="h-6 w-6 center rounded-full border-3 border-white border-opacity-50">
                 <div v-if="active" class="h-3.5 w-3.5 center rounded-full bg-white bg-opacity-50" />
             </div>
-            <span>{{ $i18n.messages[code][`languages.${code}`] }}</span>
+            <span
+                >{{ $i18n.messages[code][`languages.${code}`] }}<span v-if="progress && progress <= 95"> ({{ progress }}%)</span></span
+            >
         </div>
         <div class="flex space-x-2">
             <span>{{ $t(`languages.${code}`) }}</span>
@@ -28,6 +30,10 @@ export default Vue.extend({
         active: {
             type: Boolean,
             default: false,
+        },
+        progress: {
+            type: Number,
+            default: 100,
         },
     },
 });
--- src/components/settings/LanguageSettings.vue
+++ src/components/settings/LanguageSettings.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="flex flex-col space-y-2">
-        <LanguageOption v-for="language in languages" :key="language.code" :code="language.code" :flag="language.flagCode" :active="$i18n.locale === language.code" @select="lang = language.code" />
+        <LanguageOption v-for="language in languages.filter((lang) => lang.progress > 95).sort((a, b) => b.progress - a.progress)" :key="language.code" :code="language.code" :flag="language.flagCode" :active="$i18n.locale === language.code" @select="lang = language.code" />
+        <h3 class="text-lg font-medium pt-2">Unfinished languages</h3>
+        <LanguageOption v-for="language in languages.filter((lang) => lang.progress <= 95).sort((a, b) => b.progress - a.progress)" :key="language.code" :code="language.code" :flag="language.flagCode" :progress="language.progress" :active="$i18n.locale === language.code" @select="lang = language.code" />
     </div>
 </template>
 
@@ -14,18 +16,27 @@ export default Vue.extend({
                 {
                     code: "en",
                     flagCode: "us",
+                    progress: 100,
                 },
                 {
                     code: "de",
                     flagCode: "de",
+                    progress: 100,
                 },
                 {
                     code: "fr",
                     flagCode: "fr",
+                    progress: 77,
                 },
                 {
                     code: "nl",
                     flagCode: "nl",
+                    progress: 100,
+                },
+                {
+                    code: "zh-CN",
+                    flagCode: "cn",
+                    progress: 29,
                 },
             ],
         };
