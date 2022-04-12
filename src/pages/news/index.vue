2e60528ed2e21774e992e94b04c5fd53568a756a
OfficialCRUGG <ich@crugg.de>
2022-04-12 11:31:48 +0200

    🎨 Fixed spacing

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -2,7 +2,7 @@
     <Container>
         <div class="bg-primary bg-opacity-15 rounded-lg flex space-x-3 p-3 items-center mt-3 mb-5">
             <div class="flex-grow">
-            <AttentionIcon :filled="true" size="50" />
+                <AttentionIcon :filled="true" size="50" />
             </div>
             <div class="flex flex-col items-start">
                 <h2 class="text-white font-semibold text-lg">{{ $t("news.notReleased.heading") }}</h2>
