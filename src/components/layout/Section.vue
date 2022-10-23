f8c88c6533f5880165df468bb22e6dcff6eab1d0
OfficialCRUGG <ich@crugg.de>
2022-10-23 17:25:01 +0200

    ✨ Added noPadding prop to Sections

--- src/components/layout/Section.vue
+++ src/components/layout/Section.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="bg-dark-800 rounded-lg shadow px-4 py-2">
+    <div :class="`bg-dark-800 rounded-lg shadow ${noPadding ? '' : 'px-4 py-2'}`">
         <slot />
     </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "Section",
+    props: {
+        noPadding: {
+            type: Boolean,
+            default: false,
+        },
+    },
+});
+</script>
