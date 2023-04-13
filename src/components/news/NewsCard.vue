20ed561008a19d62813ad74fc536508aef5abb68
OfficialCRUGG <ich@crugg.de>
2023-04-14 00:36:25 +0200

    fix(news): weird photo placement in news cards

--- src/components/news/NewsCard.vue
+++ src/components/news/NewsCard.vue
@@ -1,9 +1,13 @@
 <template>
   <div
-    class="w-full rounded-md shadow min-h-40 lg:min-h-45 xl:min-h-50 flex items-end justify-start text-white p-2 pb-2 hover:pb-4 transition-all duration-250 cursor-pointer news-card"
-    :style="`background-image: linear-gradient(180deg, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0.3) 60%, rgba(0,0,0,0.6) 100%), url('https://cms.sakuta.app/assets/${post.image.filename_disk}');`"
+    class="w-full rounded-md shadow min-h-40 lg:min-h-45 xl:min-h-50 flex items-end justify-start text-white p-2 pb-2 hover:pb-4 transition-all duration-250 cursor-pointer relative overflow-hidden news-card"
+    :style="`background-image: linear-gradient(180deg, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0.3) 60%, rgba(0,0,0,0.6) 100%)`"
     @click="open()"
   >
+    <div
+      class="absolute top-0 left-0 h-full w-full -z-1 transition-all duration-250 news-card-image"
+      :style="`background-image: url('https://cms.sakuta.app/assets/${post.image.filename_disk}');`"
+    ></div>
     <div class="flex flex-col h-full space-y-1.5">
       <p class="text-xs opacity-85 flex-grow whitespace-nowrap h-full flex-grow text-right">
         {{ $moment.unix(publishedDate).locale($i18n.locale).fromNow() }}
@@ -45,13 +49,14 @@ export default Vue.extend({
 </script>
 
 <style>
-.news-card {
-  background-size: 100% auto;
-  background-position: 50% 50%;
+.news-card-image {
+  background-size: cover;
+  background-position: center;
   background-repeat: no-repeat;
+  transform: scale(1);
 }
 
-.news-card:hover {
-  background-size: 105% auto;
+.news-card:hover .news-card-image {
+  transform: scale(1.05);
 }
 </style>
